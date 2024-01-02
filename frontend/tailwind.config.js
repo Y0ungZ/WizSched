d6801d1091c97bb47891fe662082304fea78bafe
at8in <at8in@naver.com>
2024-01-02 17:46:37 +0900

    chore: tailwind config - colors, fontFamily

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,20 @@
 export default {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      red: '#FC6B85',
+      orange: '#FDB35C',
+      yellow: '#F2EC67',
+      green: '#50C69C',
+      blue: '#0087EB',
+      navy: '#000AB2',
+      gray: '#D4D0C7',
+      black: '#292929',
+    },
+    fontFamily: {
+      default: ['ChosunGu', 'sans-serif'],
+      accent: ['Busan', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
