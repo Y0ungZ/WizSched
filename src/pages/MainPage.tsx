05a4b5d6665602141d5492a5949b71809a992ef4
Y0ungZ <at8in@naver.com>
2024-02-27 22:06:26 +0900

    style: 스타일 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -7,16 +7,16 @@ const MainPage = () => {
 
   return (
     <div className="headerMarginContainer flex-col justify-evenly bg-black">
-      <div className="flex min-h-44 w-9/12 items-center justify-center rounded bg-white text-center shadow-xl shadow-green">
+      <div className="my-4 flex min-h-44 w-9/12 items-center justify-center rounded bg-white text-center shadow-xl shadow-green">
         {user ? <UserProfileSection user={user} /> : <span>로그인이 필요합니다.</span>}
       </div>
-      <div className="min-h-20 w-9/12 flex-col items-center justify-center rounded bg-white text-center shadow-xl shadow-green">
+      <div className="my-4 h-fit w-9/12 flex-col items-center justify-center rounded bg-white py-4 text-center shadow-xl shadow-green">
         <h2>캘린더</h2>
         <Button variant="contained" color="primary">
           만들기
         </Button>
       </div>
-      <div className="flex-col items-center justify-center text-center">
+      <div className="my-4 flex-col items-center justify-center text-center">
         <h2 className="textShadowYellow mb-3 font-accent text-2xl text-yellow">반복 일정</h2>
         <Button variant="contained" color="primary">
           만들기
