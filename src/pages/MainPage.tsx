1b10ff4c9403f05bcbe4d5ff28268c42986a9719
Y0ungZ <at8in@naver.com>
2024-02-27 22:07:55 +0900

    feat: Calendar 컴포넌트

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,5 @@
 import Button from '@/components/button/Button';
+import Calendar from '@/components/calendar/Calendar';
 import UserProfileSection from '@/components/sections/UserProfileSection';
 import { useAuthContext } from '@/contexts/AuthProvider';
 
@@ -10,12 +11,12 @@ const MainPage = () => {
       <div className="my-4 flex min-h-44 w-9/12 items-center justify-center rounded bg-white text-center shadow-xl shadow-green">
         {user ? <UserProfileSection user={user} /> : <span>로그인이 필요합니다.</span>}
       </div>
-      <div className="my-4 h-fit w-9/12 flex-col items-center justify-center rounded bg-white py-4 text-center shadow-xl shadow-green">
-        <h2>캘린더</h2>
+      <section className="my-4 h-fit w-9/12 flex-col items-center justify-center rounded bg-white py-4 text-center shadow-xl shadow-green">
+        <Calendar />
         <Button variant="contained" color="primary">
           만들기
         </Button>
-      </div>
+      </section>
       <div className="my-4 flex-col items-center justify-center text-center">
         <h2 className="textShadowYellow mb-3 font-accent text-2xl text-yellow">반복 일정</h2>
         <Button variant="contained" color="primary">
