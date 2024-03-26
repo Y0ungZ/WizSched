d393874c930134be252e0b1d1316bc8db5038d35
Y0ungZ <at8in@naver.com>
2024-03-27 05:49:49 +0900

    feat: QueryClientProvider 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
@@ -5,10 +6,14 @@ import AuthProvider from '@/contexts/AuthProvider';
 import router from '@/router';
 import '@/index.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
